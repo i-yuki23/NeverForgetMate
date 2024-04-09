79e4a2ae630ab60b32d279ded65221ab95a2a514
i-yuki23 <yuki.ito.s7@dc.tohoku.ac.jp>
2024-04-09 17:49:00 +0900

    gitignore mysql_data

--- src/views/index.php
+++ src/views/index.php
@@ -1,15 +1,9 @@
-<form id="locationForm" method="post">
-    <label for="locationName">Location Name:</label>
-    <input type="text" id="locationName" name="locationName" required>
-    <input type="submit" value="Submit">
-</form>
+<a href="registerLocation.php">Register Location</a>
+<div>
+    <label for="isInside">Check if you are in this location:</label>
+    <button id="checkButton" name="isInside">Check</button>
+</div>
 
-<label for="isInside">Check if you are in this location:</label>
-<button id="checkButton" name="isInside">Check</button>
-<?php if ($_SERVER["REQUEST_METHOD"] == "POST") : ?>
-    <?php $locationName = htmlspecialchars($_POST['locationName']);?>
-    <?php echo getHomeInformation($locationName); ?>
-<?php endif; ?>
 
 <p id="demo"></p>
 <script src="js/geolocation.js"></script>
---
+++ src/views/registerLocation.php
@@ -0,0 +1,5 @@
+<form id="locationForm" method="post">
+    <label for="locationName">Location Name:</label>
+    <input type="text" id="locationName" name="locationName" required>
+    <input type="submit" value="Submit">
+</form>
