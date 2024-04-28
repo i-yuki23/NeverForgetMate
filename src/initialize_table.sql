ffdd43d37597f8539fc6c466a414acafd5e994d8
i-yuki23 <yuki.ito.s7@dc.tohoku.ac.jp>
2024-04-28 15:27:01 +0900

    register one location data for one user

--- src/initialize_table.sql
+++ src/initialize_table.sql
@@ -1,4 +1,3 @@
-DROP TABLE IF EXISTS `Items`;
 DROP TABLE IF EXISTS `UserLocations`;
 
 CREATE TABLE `UserLocations` (
@@ -11,15 +10,4 @@ CREATE TABLE `UserLocations` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `Items` (
-  `item_id` int NOT NULL AUTO_INCREMENT,
-  `item_name` varchar(255) DEFAULT NULL,
-  `user_id` int DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`item_id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `items_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `UserLocations` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
